ac55dc02df50fb8c21f74ca3920c34e21bb7ad46
nezumin <nezumin@protonmail.com>
2022-09-09 20:01:00 +0700

    Added dialog branching system

--- ui/Dialog.gd
+++ ui/Dialog.gd
@@ -1,15 +1,21 @@
 extends Panel
 
-onready var speaker  = get_node("VBoxContainer/Speaker")
-onready var message  = get_node("VBoxContainer/Message")
-onready var sep_line = get_node("VBoxContainer/HSeparator")
-onready var tween    = get_node("Tween")
+onready var container = get_node("ScrollContainer/VBoxContainer")
+onready var speaker   = get_node("ScrollContainer/VBoxContainer/Speaker")
+onready var message   = get_node("ScrollContainer/VBoxContainer/Message")
+onready var sep_line  = get_node("ScrollContainer/VBoxContainer/HSeparator")
+onready var tween     = get_node("Tween")
 
 export(float) var char_per_sec = 20.0
 
 var data     : DialogScript
 var scenario : Array
 var scenario_index := 0
+var choice_var   := ""
+var button_nodes := Array() 
+
+signal line_ended
+signal dialog_ended
 
 func loadDialog(file: String, label: String):
 	data = DialogScript.loadDialog(file)
@@ -22,11 +28,17 @@ func loadDialog(file: String, label: String):
 	
 	readLine()
 
+func _ready():
+	var _connect = tween.connect("tween_all_completed", self, "emit_signal", ["line_ended"])
+
+func _gui_input(event):
+	if event.is_action("ui_accept") or event.is_action("left_click"):
+		advance()
+
 func advance():
 	if tween.is_active():
-		tween.stop_all()
-		message.percent_visible = 1.0
-	else:
+		endLine()
+	elif !choice_var.empty():
 		scenario_index += 1
 		readLine()
 
@@ -52,9 +64,13 @@ func evalArray(array: Array):
 	var head = array.pop_front()
 	match head:
 		DialogScript.Command.PRINT:
-			printMessage(array[0])
+			printMessage(array)
 		DialogScript.Command.SAY:
-			sayMessage(array[0], array[1])
+			sayMessage(array.pop_front(), array)
+		DialogScript.Command.CHOOSE:
+			var variable = array.pop_front()
+			var prompt   = array.pop_front()
+			choose(variable, prompt, array)
 		DialogScript.Command.QUIT:
 			close()
 		
@@ -98,21 +114,24 @@ func evalArray(array: Array):
 			message.bbcode_text = err_msg
 
 func close():
-	pass
+	emit_signal("dialog_ended")
 
-func printMessage(text: String):
+func printMessage(args: Array):
 	speaker.visible  = false
 	sep_line.visible = false
-	printText(text)
+	printText(args)
 
-func sayMessage(chara_name: String, text: String):
+func sayMessage(chara_name: String, args: Array):
 	speaker.visible  = true
 	sep_line.visible = true
-	
 	speaker.bbcode_text = chara_name
-	printText(text)
+	printText(args)
 
-func printText(text: String):
+func printText(args: Array):
+	var text := ""
+	for arg in args:
+		text += str(eval(arg)) + " "
+
 	message.percent_visible = 0.0
 	message.bbcode_text = text
 	
@@ -120,3 +139,36 @@ func printText(text: String):
 	tween.interpolate_property(message, "percent_visible",
 	0.0, 1.0, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
+
+func choose(var_name: String, prompt: Array, choice: Array):
+	choice_var = var_name
+	printMessage(prompt)
+	yield(self, "line_ended")
+	
+	for c_arr in choice:
+		assert(typeof(c_arr) == TYPE_ARRAY)
+		if c_arr.size() == 3:
+			var condition = c_arr.pop_back()
+			if !eval(condition):
+				continue
+		addChoice(eval(c_arr[0]), eval(c_arr[1]))
+
+func addChoice(value, text: String):
+	var button = Button.new()
+	button.text = text
+	button_nodes.push_back(button)
+	container.add_child(button)
+	var _connect = button.connect("button_down", self, "onChoiceMade", [value])
+
+func onChoiceMade(value):
+	data.setVar(choice_var, value)
+	choice_var = ""
+	for button in button_nodes:
+		button.queue_free()
+	button_nodes.clear()
+	advance()
+
+func endLine():
+	tween.stop_all()
+	message.percent_visible = 1.0
+	emit_signal("line_ended")
